a5d3168a1d31dcfed95a6618dc18f99a46b0a78e
Nguyễn Ngọc Hải <ngochai220998@gmail.com>
2019-02-21 15:27:11 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+let express = require('express');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+
+app.use('/assets', express.static(__dirname + '/public'));
+app.set('view engine','ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(morgan('dev'));
+
+
+
+
+app.get('/',(req,res) => {
+    res.render('home.ejs');
+});
+app.get('/chitiet',(req,res) => {
+    res.render('post.ejs');
+})
+
+
+
+app.listen(PORT, function(){
+    console.log("Application listenning on port: " + PORT);
+})
