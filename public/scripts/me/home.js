901ed762a1a4e84a618e41c26eaaea6af81c6483
Nguyễn Ngọc Hải <ngochai220998@gmail.com>
2019-02-21 21:26:34 +0700

    make it better

--- public/scripts/me/home.js
+++ public/scripts/me/home.js
@@ -15,11 +15,42 @@ document.addEventListener("DOMContentLoaded",function(){
         }
         
 
-        
+
 
     }
     // END HEADER HOME
 
+
+     //** CONTAINER___Content */
+     {
+        //** Conent WHY__US */
+        {
+            let whyUSNode = document.querySelectorAll('.container__why__bot ul li');
+            let whyUsSlide = document.querySelectorAll('.container .container__why__container__content');
+            let imageSlide = document.querySelectorAll('.container .container__why__container__content__image');
+
+            //console.log(imageSlide[0].getAttribute('link'));
+            for(let i = whyUSNode.length - 1; i >= 0 ; --i){
+                whyUSNode[i].onclick = function(){
+                    for (let j = 0; j < whyUsSlide.length; j++) {
+                        whyUsSlide[j].style.display = "none";  
+                    }
+                    for(let j = 0 ; j < whyUSNode.length ; ++j){
+                        whyUSNode[j].classList.remove('backgroundActive');
+                    }
+                    this.classList.add('backgroundActive');
+                    whyUsSlide[i].style.display = "block"; 
+                }
+            }
+            // init slide default
+            (function showWhyUsSlide() {
+                whyUsSlide[0].style.display = "block";
+                whyUSNode[0].classList.add('backgroundActive');
+            })();
+        }
+        //END_CONTEN_WHY_US
+    }
+    //END___CONTAINER___Content
 
 
 
