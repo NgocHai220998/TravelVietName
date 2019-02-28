470f8410d0a420996f5e08341703e68793e7e0fe
Nguyễn Ngọc Hải <ngochai220998@gmail.com>
2019-02-28 08:53:28 +0700

    Box report done 50%

--- public/scripts/me/post.js
+++ public/scripts/me/post.js
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded",function(){
     }
     
 
-
     /////////////////////////////////////////////////////
     // slide
 
@@ -64,6 +63,80 @@ document.addEventListener("DOMContentLoaded",function(){
         }
 
         showSlide();
+    }
+
+
+    /////////////////////////////////////////////////////
+    // show image
+    {
+        let Image = document.querySelector('.container__show__content .container__show__content__image ._img');
+        let listImage = document.querySelectorAll('.container__show__content .list-image ._img');
+        let nodeLeft = document.querySelector('.container__show__content__image .fas.fa-angle-left');
+        let nodeRight = document.querySelector('.container__show__content__image .fas.fa-angle-right');
+        let show = document.querySelector('.container__show');
+        let background = document.querySelector('.container__show .container__show__background');
+        let postImage = document.querySelectorAll('.image__post .image__post__img');
+        let btnMore = document.getElementById('btn-more-img');
+        
+        background.onclick = function(){
+            show.style.display = "none";
+        }
+        //console.log(postImage);
+        nodeLeft.onclick = function(){
+            let index = parseInt(Image.getAttribute('index'));
+            if(index === listImage.length - 1){
+                index = 0;
+            }else{
+                index = index + 1;
+            }
+            Image.setAttribute('index',index);
+            Image.setAttribute('src',listImage[index].getAttribute('src'));
+        }
+        nodeRight.onclick = function(){
+            let index = parseInt(Image.getAttribute('index'));
+            if(index === 0){
+                index = listImage.length - 1;
+            }else{
+                index = index - 1;
+            }
+            Image.setAttribute('index',index);
+            Image.setAttribute('src',listImage[index].getAttribute('src'));
+        }
+
+        for(let i = 0; i < postImage.length ; ++i){
+ 
+            postImage[i].onclick = function(){
+                let index = parseInt(this.getAttribute('index'));
+                Image.setAttribute('index',index);
+                Image.setAttribute('src',this.getAttribute('src'));
+                show.style.display = "block";
+            }
+        }
+        btnMore.onclick = function(){
+            Image.setAttribute('index',0);
+            Image.setAttribute('src',listImage[0].getAttribute('src'));
+            show.style.display = "block";
+        }
+    }
+
+
+
+    /////////////////////////////////////////////////////
+    // Review
+    {
+        let comment = document.getElementById('comment');
+        let containerComment = document.getElementById('text');
+        let report = document.querySelector('.report');
+        comment.onclick = function(){
+            containerComment.classList.add('text--comment');
+        }
+
+        comment.onblur = function(){
+            containerComment.classList.remove('text--comment');
+        }
+
+
+        //console.log(containerComment);
     }
     
 
