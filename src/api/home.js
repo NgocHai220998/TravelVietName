5b877711e19c960779ee3d1330397cce5d305b83
Nguyễn Ngọc Hải <ngochai220998@gmail.com>
2019-04-09 16:51:00 +0700

    Bcrypt

--- src/api/home.js
+++ src/api/home.js
@@ -6,10 +6,10 @@ let Users = require('../models/userModel.js');
 module.exports = function(app){
 
 
-    app.get('/home',(req,res) => {
+    app.get('/',(req,res) => {
         res.render('home.ejs');
     });
-    app.post('/home',(req,res)=>{
+    app.post('/',(req,res)=>{
         res.cookie('email',req.body.email).render('home.ejs');
     });
 
