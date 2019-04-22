002cda3377a8b76f4011a8f66574d165953f204d
Nguyễn Ngọc Hải <ngochai220998@gmail.com>
2019-04-22 08:44:16 +0700

    done form add detail

--- src/api/addDetail.js
+++ src/api/addDetail.js
@@ -1,12 +1,32 @@
 
 
 
+let userModel = require('../models/userModel.js');
+let multer = require('multer');
+
+
+let storage = multer.diskStorage({  
+  destination: (req, file, cb) => {
+    cb(null, 'public/images/uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+let upload = multer({storage: storage});
+
+
 module.exports = function(app){
 
 
   app.get('/api/ngochai220998/adddetail',(req,res)=>{
     res.render('addDetail');
-  })
+  });
+
+  app.post('/api/adddetail',upload.single("file"),(req,res)=>{
+    console.log(req.body);
+    res.send(req.body);
+  });
 
 
 
